ae03a0d890f24dd107601114811dbbe6939dc681
Igor Duarte <igor.gsduarte@gmail.com>
2022-02-17 15:07:04 -0300

    Rebuild

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import Query from './Query';
 import CancellationToken from './CancellationToken';
-
-import { delay, pipe, compose, race, all, step, after, loop, loopWhile, map, filter } from './combinators';
 import Channel, { spawn, go } from './channel';
 import Process, { runProcess } from './process'
-import {  isFunction, isIterator, isObject, isPromise, getIterator } from './types';
 
-export {  Query, CancellationToken, runProcess, delay, pipe, compose, race, all, step, after, loop, loopWhile, map, filter, isFunction, isIterator, isObject, isPromise, getIterator, spawn, go };
-export default { Process, Channel, Query, CancellationToken, getIterator };
+export { delay, pipe, compose, race, all, step, after, loop, loopWhile, map, filter } from './combinators';
+export {  isFunction, isIterator, isObject, isPromise, getIterator } from './types';
+export {  Query, CancellationToken, runProcess, spawn, go };
+export default { Process, Channel, Query, CancellationToken };
