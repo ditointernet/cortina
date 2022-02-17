97ba48e26a47d0a5ec4a055aa44b666942da2891
Igor Duarte <igor.gsduarte@gmail.com>
2022-02-17 14:52:26 -0300

    Rebuild

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+var path = require('path');
+
+module.exports = {
+   entry: {
+      app: './src/index.js'
+   },
+   output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: 'index.js'
+   },
+   module: {
+      rules: [
+         {
+            test: /\.js$/,
+            include: path.resolve(__dirname, 'src'),
+            loader: 'babel-loader',
+            options: { presets: ['@babel/env'] }
+         }
+      ]
+   }
+};
