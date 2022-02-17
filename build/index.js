6046ded0a2eba0e89e6e960ca4b3795179bed579
Igor Duarte <igor.gsduarte@gmail.com>
2022-02-17 11:46:32 -0300

    Rebuild

--- build/index.js
+++ build/index.js
@@ -3,135 +3,15 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.CancellationToken = exports.Query = exports.isPromise = exports.isObject = exports.isIterator = exports.isFunction = exports.Process = exports.runProcess = exports.filter = exports.map = exports.loopWhile = exports.loop = exports.after = exports.step = exports.all = exports.race = exports.compose = exports.pipe = exports.delay = exports.go = exports.spawn = undefined;
-
-var _channel = require('./channel');
-
-Object.defineProperty(exports, 'spawn', {
-  enumerable: true,
-  get: function () {
-    return _channel.spawn;
-  }
-});
-Object.defineProperty(exports, 'go', {
-  enumerable: true,
-  get: function () {
-    return _channel.go;
-  }
-});
-
-var _combinators = require('./combinators');
-
-Object.defineProperty(exports, 'delay', {
-  enumerable: true,
-  get: function () {
-    return _combinators.delay;
-  }
-});
-Object.defineProperty(exports, 'pipe', {
-  enumerable: true,
-  get: function () {
-    return _combinators.pipe;
-  }
-});
-Object.defineProperty(exports, 'compose', {
-  enumerable: true,
-  get: function () {
-    return _combinators.compose;
-  }
-});
-Object.defineProperty(exports, 'race', {
-  enumerable: true,
-  get: function () {
-    return _combinators.race;
-  }
-});
-Object.defineProperty(exports, 'all', {
-  enumerable: true,
-  get: function () {
-    return _combinators.all;
-  }
-});
-Object.defineProperty(exports, 'step', {
-  enumerable: true,
-  get: function () {
-    return _combinators.step;
-  }
-});
-Object.defineProperty(exports, 'after', {
-  enumerable: true,
-  get: function () {
-    return _combinators.after;
-  }
-});
-Object.defineProperty(exports, 'loop', {
-  enumerable: true,
-  get: function () {
-    return _combinators.loop;
-  }
-});
-Object.defineProperty(exports, 'loopWhile', {
-  enumerable: true,
-  get: function () {
-    return _combinators.loopWhile;
-  }
-});
-Object.defineProperty(exports, 'map', {
-  enumerable: true,
-  get: function () {
-    return _combinators.map;
-  }
-});
-Object.defineProperty(exports, 'filter', {
-  enumerable: true,
-  get: function () {
-    return _combinators.filter;
-  }
-});
+exports.getIterator = exports.CancellationToken = exports.Query = exports.Channel = exports.Process = undefined;
 
 var _process = require('./process');
 
-Object.defineProperty(exports, 'runProcess', {
-  enumerable: true,
-  get: function () {
-    return _process.runProcess;
-  }
-});
-Object.defineProperty(exports, 'Process', {
-  enumerable: true,
-  get: function () {
-    return _process.Process;
-  }
-});
-
-var _types = require('./types');
-
-Object.defineProperty(exports, 'isFunction', {
-  enumerable: true,
-  get: function () {
-    return _types.isFunction;
-  }
-});
-Object.defineProperty(exports, 'isIterator', {
-  enumerable: true,
-  get: function () {
-    return _types.isIterator;
-  }
-});
-Object.defineProperty(exports, 'isObject', {
-  enumerable: true,
-  get: function () {
-    return _types.isObject;
-  }
-});
-Object.defineProperty(exports, 'isPromise', {
-  enumerable: true,
-  get: function () {
-    return _types.isPromise;
-  }
-});
-
 var _process2 = _interopRequireDefault(_process);
+
+var _channel = require('./channel');
+
+var _channel2 = _interopRequireDefault(_channel);
 
 var _Query = require('./Query');
 
@@ -141,8 +21,13 @@ var _CancellationToken = require('./CancellationToken');
 
 var _CancellationToken2 = _interopRequireDefault(_CancellationToken);
 
+var _types = require('./types');
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+exports.Process = _process2.default;
+exports.Channel = _channel2.default;
 exports.Query = _Query2.default;
 exports.CancellationToken = _CancellationToken2.default;
-exports.default = { Process: _process2.default, Channel: _channel.Channel, Query: _Query2.default, CancellationToken: _CancellationToken2.default, getIterator: _types.getIterator };
+exports.getIterator = _types.getIterator;
+exports.default = { Process: _process2.default, Channel: _channel2.default, Query: _Query2.default, CancellationToken: _CancellationToken2.default, getIterator: _types.getIterator };
--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 import Process from './process';
+import Channel from './channel';
 import Query from './Query';
 import CancellationToken from './CancellationToken';
-import { Channel } from './channel';
 import { getIterator } from './types';
 
-export {spawn, go, } from './channel';
-export {delay, pipe, compose, race, all, step, after, loop, loopWhile, map, filter} from './combinators';
-export { runProcess, Process } from './process'
-export {  isFunction, isIterator, isObject, isPromise } from './types';
-
-export {  Query, CancellationToken };
+export { Process, Channel, Query, CancellationToken, getIterator };
 export default { Process, Channel, Query, CancellationToken, getIterator };
